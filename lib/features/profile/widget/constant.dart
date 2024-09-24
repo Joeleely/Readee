a8f3c669e1b126b3024b3e9ca44186759cdf53b6
parnlees <yanisa0439@gmail.com>
2024-09-24 14:34:52 +0700

    editProfilePage

--- lib/features/profile/widget/constant.dart
+++ lib/features/profile/widget/constant.dart
@@ -1,6 +1,5 @@
-const String tFristName = 'FristName';
+const String tFirstName = 'FirstName';
 const String tLastName = 'LastName';
 const String tUsername = 'Username';
 const String tEmail = 'Email';
 const String tGender = 'Gender';
-const String tHintWaitingBackend = 'waiting backend';
