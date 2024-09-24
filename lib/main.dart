8f620a241df5a3046b7ab6b08f45dd8c27ac40e7
parnlees <yanisa0439@gmail.com>
2024-09-24 12:02:04 +0700

    profile structure updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,26 @@
 import 'package:flutter/material.dart';
-import 'package:readee_app/features/match/match.dart';
-import 'package:readee_app/home.dart';
-import 'package:readee_app/pages/logo.dart';
-import 'package:readee_app/features/auth/register.dart';
+import 'package:get/get.dart';
+import 'package:readee_app/features/profile/profile.dart';
 
 void main() {
-  runApp(const HomePage());
+  runApp(const MyApp());
 }
 
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: const Color.fromARGB(255, 140, 226, 255)),
+        useMaterial3: true,
+        scaffoldBackgroundColor: const Color.fromARGB(255, 243, 252, 255),
+      ),
+      home: ProfilePage(),
+    );
+  }
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,6 +9,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:readee_app/home.dart';
 
+import 'package:readee_app/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
