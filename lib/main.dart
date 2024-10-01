79e1a239ffb89d9536b4d4935a44ecf77baf7056
fahsaifst <98171729+fahsaifst@users.noreply.github.com>
2024-10-01 12:49:49 +0700

    fix main

--- lib/main.dart
+++ lib/main.dart
@@ -4,24 +4,6 @@ import 'package:readee_app/features/profile/profile.dart';
 import 'package:readee_app/home.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const HomePage());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return GetMaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-            seedColor: const Color.fromARGB(255, 140, 226, 255)),
-        useMaterial3: true,
-        scaffoldBackgroundColor: const Color.fromARGB(255, 243, 252, 255),
-      ),
-      home: HomePage(),
-    );
-  }
-}
