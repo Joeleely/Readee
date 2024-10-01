7385d52c702cb07e98d7cf1b9c4923408631e77f
fahsaifst <98171729+fahsaifst@users.noreply.github.com>
2024-10-01 20:53:15 +0700

    swipe card

--- lib/features/match/pages/book_info.dart
+++ lib/features/match/pages/book_info.dart
@@ -1,14 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:readee_app/features/match/model/book_details.dart';
 import 'package:readee_app/typography.dart';
 
-class BookInfoPage extends StatefulWidget {
-  const BookInfoPage({super.key});
+class BookInfoPage extends StatelessWidget {
+  const BookInfoPage({super.key, required this.book});
+  final Book book;
 
-  @override
-  State<BookInfoPage> createState() => _BookInfoPageState();
-}
-
-class _BookInfoPageState extends State<BookInfoPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,7 +22,7 @@ class _BookInfoPageState extends State<BookInfoPage> {
                 SizedBox(
                   width: MediaQuery.of(context).size.width * 0.7,
                   child: Text(
-                    'The Seven Husbands of Evelyn Hugo',
+                    book.title,
                     maxLines: 2,
                     style: TypographyText.h2(Colors.black),
                   ),
@@ -34,8 +31,8 @@ class _BookInfoPageState extends State<BookInfoPage> {
                   height: 5,
                 ),
                 Text(
-                  'Taylor Jenkins Reid',
-                  style: TypographyText.b4(Colors.grey),
+                  book.author,
+                  style: TypographyText.b3(Colors.grey),
                 )
               ],
             ),
@@ -57,11 +54,9 @@ class _BookInfoPageState extends State<BookInfoPage> {
                 child: Container(
                   // width: MediaQuery.of(context).size.width * 0.9,
                   height: MediaQuery.of(context).size.height * 0.6,
-                  decoration: const BoxDecoration(
+                  decoration: BoxDecoration(
                       image: DecorationImage(
-                          fit: BoxFit.cover,
-                          image: NetworkImage(
-                              'https://bci.kinokuniya.com/jsp/images/book-img/97813/97813985/9781398515697.JPG'))),
+                          fit: BoxFit.cover, image: NetworkImage(book.img))),
                 ),
               ),
               const SizedBox(
@@ -80,7 +75,7 @@ class _BookInfoPageState extends State<BookInfoPage> {
                       height: 20,
                     ),
                     Text(
-                      'Aging and reclusive Hollywood movie icon Evelyn Hugo is finally ready to tell the truth about her glamorous and scandalous life. But when she chooses unknown magazine reporter Monique Grant for the job, no one is more astounded than Monique herself. Why her? Why now? Monique is not exactly on top of the world. Her husband has left her, and her professional life is going nowhere. Regardless of why Evelyn has selected her to write her biography, Monique is determined to use this opportunity to jumpstart her career.Summoned to Evelyns luxurious apartment, Monique listens in fascination as the actress tells her story. From making her way to Los Angeles in the 1950s to her decision to leave show business in the 80s, and, of course, the seven husbands along the way, Evelyn unspools a tale of ruthless ambition, unexpected friendship, and a great forbidden love. Monique begins to feel a very real connection to the legendary star, but as Evelyns story nears its conclusion, it becomes clear that her life intersects with Moniques own in tragic and irreversible ways',
+                      book.description,
                       style: TypographyText.b2(Colors.black),
                     )
                   ],
--- lib/features/match/widgets/book_card.dart
+++ lib/features/match/widgets/book_card.dart
@@ -1,142 +1,219 @@
 import 'package:flutter/material.dart';
+import 'package:readee_app/features/match/model/book_details.dart';
 import 'package:readee_app/features/match/pages/book_info.dart';
 import 'package:readee_app/typography.dart';
+import 'package:swipe_cards/swipe_cards.dart';
 
 class BookCard extends StatefulWidget {
-  const BookCard({super.key});
+  const BookCard({super.key, required this.books});
+  final List<Book> books;
 
   @override
   State<BookCard> createState() => _BookCardState();
 }
 
 class _BookCardState extends State<BookCard> {
+  final List<SwipeItem> _swipeItems = <SwipeItem>[];
+  MatchEngine? _matchEngine;
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey();
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Initialize SwipeItems with Book details
+    for (var book in widget.books) {
+      _swipeItems.add(SwipeItem(
+        content: book,
+        likeAction: () {
+          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+            content: Text("Liked ${book.title}"),
+            duration: const Duration(milliseconds: 500),
+          ));
+        },
+        nopeAction: () {
+          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+            content: Text("Nope ${book.title}"),
+            duration: const Duration(milliseconds: 500),
+          ));
+        },
+      ));
+    }
+
+    _matchEngine = MatchEngine(swipeItems: _swipeItems);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(bottom: 20),
-      child: Stack(
+    return Scaffold(
+      key: _scaffoldKey,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          ClipRRect(
-            borderRadius: BorderRadius.circular(10),
-            child: Container(
-              width: MediaQuery.of(context).size.width * 0.9,
-              height: MediaQuery.of(context).size.height * 0.8,
-              decoration: const BoxDecoration(
-                  image: DecorationImage(
-                      fit: BoxFit.cover,
-                      image: NetworkImage(
-                          'https://bci.kinokuniya.com/jsp/images/book-img/97813/97813985/9781398515697.JPG'))),
-            ),
-          ),
-          Positioned(
-            bottom: 0,
-            left: 0,
-            right: 0,
-            child: Container(
-              height: 500,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                gradient: const LinearGradient(
-                  begin: Alignment.topCenter,
-                  end: Alignment.bottomCenter,
-                  colors: [Colors.transparent, Colors.black87],
-                ),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Row(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Row(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          children: [
-                            SizedBox(
-                              width: 200,
-                              child: Text(
-                                'The Seven Husbands of Evelyn Hugo',
-                                maxLines: 2,
-                                style: TypographyText.h2(Colors.white),
-                              ),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.only(top: 6),
-                              child: Container(
-                                  padding: const EdgeInsets.symmetric(
-                                    horizontal: 8,
-                                  ),
-                                  decoration: BoxDecoration(
-                                      color: Colors.white.withOpacity(0.5),
-                                      borderRadius: const BorderRadius.all(
-                                          Radius.circular(10))),
-                                  child: Text(
-                                    '95%',
-                                    style: TypographyText.h4(Colors.white),
-                                  )),
-                            ),
-                          ],
-                        ),
-                        IconButton(
-                            onPressed: () {
-                              Navigator.of(context).push(_createRoute());
-                            },
-                            icon: const Icon(
-                              Icons.info_rounded,
-                              color: Colors.white,
-                            ))
-                      ],
-                    ),
-                    Text(
-                      'Taylor Jenkins Reid',
-                      style: TypographyText.b4(Colors.grey),
-                    ),
-                    const SizedBox(height: 5),
-                    Text(
-                      'description this is mock to check love you the seven husbands of evelyn hugo',
-                      style: TypographyText.b3(Colors.white.withOpacity(0.8)),
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        ElevatedButton(
-                          onPressed: () {},
-                          style: ElevatedButton.styleFrom(
-                              shape: const CircleBorder(),
-                              padding: const EdgeInsets.all(15),
-                              backgroundColor: Colors.white),
-                          child: const Icon(
-                            Icons.close,
-                            color: Colors.red,
-                            size: 40,
+          Expanded(
+            child: SwipeCards(
+              matchEngine: _matchEngine!,
+              itemBuilder: (BuildContext context, int index) {
+                Book book = _swipeItems[index].content;
+                return Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Stack(
+                    children: [
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(10),
+                        child: Container(
+                          // width: MediaQuery.of(context).size.width * 0.9,
+                          height: MediaQuery.of(context).size.height * 0.8,
+                          decoration: BoxDecoration(
+                            image: DecorationImage(
+                                fit: BoxFit.cover,
+                                image: NetworkImage(book.img)),
                           ),
                         ),
-                        const SizedBox(width: 50),
-                        ElevatedButton(
-                          onPressed: () {},
-                          style: ElevatedButton.styleFrom(
-                              shape: const CircleBorder(),
-                              padding: const EdgeInsets.all(15),
-                              backgroundColor: Colors.white),
-                          child: Icon(
-                            Icons.favorite,
-                            color: Colors.greenAccent[400],
-                            size: 40,
+                      ),
+                      Positioned(
+                        bottom: 0,
+                        left: 0,
+                        right: 0,
+                        child: Container(
+                          height: 200,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            gradient: const LinearGradient(
+                              begin: Alignment.topCenter,
+                              end: Alignment.bottomCenter,
+                              colors: [Colors.transparent, Colors.black87],
+                            ),
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.all(10.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: [
+                                Row(
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        SizedBox(
+                                          width: 250,
+                                          child: Text(
+                                            book.title,
+                                            style:
+                                                TypographyText.h2(Colors.white),
+                                            maxLines: 2,
+                                          ),
+                                        ),
+                                        Text(
+                                          book.author,
+                                          style:
+                                              TypographyText.b4(Colors.white),
+                                        ),
+                                        // const SizedBox(
+                                        //   width: 10,
+                                        // ),
+                                        // Padding(
+                                        //   padding:
+                                        //       const EdgeInsets.only(top: 6),
+                                        //   child: Container(
+                                        //       padding:
+                                        //           const EdgeInsets.symmetric(
+                                        //         horizontal: 8,
+                                        //       ),
+                                        //       decoration: BoxDecoration(
+                                        //           color: Colors.white
+                                        //               .withOpacity(0.6),
+                                        //           borderRadius:
+                                        //               const BorderRadius.all(
+                                        //                   Radius.circular(
+                                        //                       10))),
+                                        //       child: Text(
+                                        //         book.quality,
+                                        //         style: TypographyText.h4(
+                                        //             Colors.white),
+                                        //       )),
+                                        // ),
+                                      ],
+                                    ),
+                                    IconButton(
+                                        padding: EdgeInsets.zero,
+                                        constraints: const BoxConstraints(),
+                                        onPressed: () {
+                                          Navigator.of(context)
+                                              .push(_createRoute(book));
+                                        },
+                                        icon: const Icon(
+                                          Icons.info_rounded,
+                                          color: Colors.white,
+                                        ))
+                                  ],
+                                ),
+                                const SizedBox(height: 5),
+                                Text(
+                                  book.description,
+                                  style: TypographyText.b3(
+                                      Colors.white.withOpacity(0.8)),
+                                  overflow: TextOverflow.ellipsis,
+                                ),
+                                const SizedBox(height: 10),
+                                Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceEvenly,
+                                  children: [
+                                    ElevatedButton(
+                                      onPressed: () {
+                                        _matchEngine!.currentItem?.nope();
+                                      },
+                                      style: ElevatedButton.styleFrom(
+                                        shape: CircleBorder(),
+                                        padding: EdgeInsets.all(15),
+                                        backgroundColor: Colors.white,
+                                      ),
+                                      child: const Icon(Icons.close,
+                                          color: Colors.red, size: 40),
+                                    ),
+                                    ElevatedButton(
+                                      onPressed: () {
+                                        _matchEngine!.currentItem?.like();
+                                      },
+                                      style: ElevatedButton.styleFrom(
+                                        shape: const CircleBorder(),
+                                        padding: const EdgeInsets.all(15),
+                                        backgroundColor: Colors.white,
+                                      ),
+                                      child: Icon(Icons.favorite,
+                                          color: Colors.greenAccent[400],
+                                          size: 40),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
                           ),
                         ),
-                      ],
-                    )
-                  ],
-                ),
-              ),
+                      ),
+                    ],
+                  ),
+                );
+              },
+              onStackFinished: () {
+                ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                  content: Text("Stack Finished"),
+                  duration: Duration(milliseconds: 500),
+                ));
+              },
+              itemChanged: (SwipeItem item, int index) {
+                print("Book: ${item.content.title}, index: $index");
+              },
+              leftSwipeAllowed: true,
+              rightSwipeAllowed: true,
+              upSwipeAllowed: false,
+              fillSpace: true,
             ),
           ),
         ],
@@ -145,10 +222,10 @@ class _BookCardState extends State<BookCard> {
   }
 }
 
-Route _createRoute() {
+Route _createRoute(Book book) {
   return PageRouteBuilder(
     pageBuilder: (context, animation, secondaryAnimation) =>
-        const BookInfoPage(),
+        BookInfoPage(book: book),
     transitionsBuilder: (context, animation, secondaryAnimation, child) {
       const begin = Offset(0.0, 1.0);
       const end = Offset.zero;
