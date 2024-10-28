f370907a5fe1606214238fa1d12b64a4bf08ac29
fahsaifst <98171729+fahsaifst@users.noreply.github.com>
2024-10-28 18:50:25 +0700

    review page api

--- lib/features/profile/review.dart
+++ lib/features/profile/review.dart
@@ -39,7 +39,7 @@ class _ReviewPageState extends State<ReviewPage> {
               onTap: () {
                 Navigator.push(
                   context,
-                  MaterialPageRoute(builder: (context) => YourRatingPage()),
+                  MaterialPageRoute(builder: (context) => YourRatingPage(userId: 1,)),
                 );
               },
             ),
